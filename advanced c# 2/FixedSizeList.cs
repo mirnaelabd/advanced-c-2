7271f6f5a7f0e439be349d27b9092e6298fc7797
mirna <mirna@DESKTOP-NVAPE0C>
2024-08-06 14:47:36 +0300

    Add project files.

---
+++ advanced c# 2/FixedSizeList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_c__2
+{
+    public class FixedSizeList<T>
+    {
+     private  List<T> list;
+
+        public int Count => list.Count;
+        
+        public FixedSizeList(int cacity )
+        {
+           list = new List<T>(cacity);
+           
+            
+          
+        }
+        public void Add_element(T item)
+        {
+
+           if (list.Capacity >= list.Count)
+                list.Add(item);
+            else
+                Console.WriteLine("can't add a element list is full");
+
+       }
+        public void get( int index)
+        {
+            if(index < 0 || index >= list.Count)
+            {
+                Console.WriteLine("out of range ");
+            }
+            else
+
+                Console.WriteLine($"{list[index]}");
+            
+
+
+        }
+
+
+
+    }
+}
---
+++ advanced c# 2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace advanced_c__2
+{
+    internal class Program
+    {
+        public static void Reverse(ArrayList list)
+        {
+            int num1 = 0;
+            int num2 = list.Count - 1;
+            if ( list is not null )
+            while (num1 < num2)
+            {
+
+                object temp = list[num1];
+                list[num1] = list[num2];
+                list[num2] = temp;
+                num1++;
+                num2--;
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            #region 11111
+            /*     ArrayList numbers = new ArrayList();
+                 numbers.Add(1);
+                 numbers.Add(2);
+                 numbers.Add(3);
+                 numbers.Add(4);
+                 Console.WriteLine("Original list:");
+                 foreach (int number in numbers)
+                 {
+                     Console.Write(number + " ");
+                 }
+                 Console.WriteLine();
+
+                 Reverse(numbers);
+
+                 Console.WriteLine("Reversed list:");
+                 foreach (int number in numbers)
+                 {
+                     Console.Write(number + " ");
+                 }
+                 Console.WriteLine(); */
+            #endregion
+
+            #region 22222222
+            /*       List<int> list = new List<int>() { 1,2 ,3 , 4 ,5 ,6 };
+                   List<int> newlist = new List<int>(); 
+                   for (int i = 0; i < list.Count; i++)
+                   {
+                       if (list[i] % 2 == 0)
+                           newlist.Add(list[i]);
+
+                   }
+                   Console.WriteLine("the orginal list ");
+                   foreach (int i in list)
+                   {
+                       Console.WriteLine(i);
+
+                   }
+                   Console.WriteLine("the new list ");
+                   foreach (int i in newlist)
+                   {
+                       Console.WriteLine(i);
+
+                   }*/
+
+            #endregion
+
+            #region 3333333333333
+      /*      FixedSizeList<int> list = new FixedSizeList<int>(3);
+            list.Add_element(0);
+            list.Add_element(2);   
+            list.Add_element(4);
+            list.Add_element(6);
+            list.get(6);
+            */
+
+
+            #endregion
+
+
+        }
+
+
+
+    }
+    }
+
+
+
+
